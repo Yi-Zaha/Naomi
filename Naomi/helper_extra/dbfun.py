16cbe4a8736882a4d3c53954e2d0f71841de855f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-08-07 20:20:13 +0530

    Update dbfun.py

--- Naomi/helper_extra/dbfun.py
+++ Naomi/helper_extra/dbfun.py
@@ -50,6 +50,7 @@ welcomedb = db.welcome_text
 blacklist_filtersdb = db.blacklistFilters
 pipesdb = db.pipes
 sudoersdb = db.sudoers
+nsfwdb = db.nsfw
 blacklist_chatdb = db.blacklistChat
 restart_stagedb = db.restart_stage
 flood_toggle_db = db.flood_toggle
@@ -786,3 +787,22 @@ async def get_nsfw_status(chat_id: int) -> bool:
     if not text:
         return False
     return text["allowed"]
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return nsfwdb.insert_one({"chat_id": chat_id})
