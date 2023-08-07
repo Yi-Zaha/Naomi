5bdccbd1ccac8a6cea1c8a26950722fbb37fcba2
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-08-07 20:03:07 +0530

    Update mongo.py

--- Naomi/mongo.py
+++ Naomi/mongo.py
@@ -6,16 +6,16 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Naomi.confing import get_int_key, get_str_key
 
-MONGO_DB_URI="mongodb+srv://f2l:f2l@cluster0.fjjge1y.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI="mongodb+srv://kagut:kagut@cluster0.hol7gj5.mongodb.net/?retryWrites=true&w=majority"
 MONGO_PORT = 27017
-MONGO_DB = "f2l"
+MONGO_DB = "naomi"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["raj"]
+db = client["naomi"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
